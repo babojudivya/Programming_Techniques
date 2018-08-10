5ffe45f80d021c28352437a21ea6b06f84609ba5
erickbarcenas <42219451+erickbarcenas@users.noreply.github.com>
2018-08-09 20:56:06 -0500

    practica_0

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a=5;
+            int b=10;
+            int a;
+            bool condition = true;
+            
+
+
+            /*int num = 2;
+            double num_2 = 30.33;
+            Boolean status = true;
+            Boolean status_2 = false;
+            String message = "Hello";
+            
+            */
+
+            /* Console.WriteLine("Hello, World");
+             Console.Write(num);
+             Console.Write("\n" + num_2);
+             Console.Write("\n"+status);
+             Console.Write("\n"+"Boolean\n" + status_2);
+             Console.Write("\n" + message);
+             Console.Read();
+
+             */
+
+            if (condition)
+            {
+                a = Convert.ToInt32(Console.ReadLine());
+                a = a+1;
+                //Console.WriteLine("The variable is set to true.");
+                Console.WriteLine("a vale " + a);
+                Console.ReadLine();
+
+            }
+            else
+            {
+                a = Convert.ToInt32(Console.ReadLine());
+                a = b + 1;
+                //Console.WriteLine("The variable is set to false.");
+                Console.WriteLine("a vale " + a+5);
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+
+}
+
+
+
+

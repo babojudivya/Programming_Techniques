a8f104353f145baca7c27eab81616ec3feb0c015
Erick I Bárcenas M <42219451+erickbarcenas@users.noreply.github.com>
2018-08-16 20:49:22 -0500

    Herencia

---
+++ Poo/Principal_Herencia/Principal_Herencia/Figura_Geometrica.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principal_Herencia
+{
+    public class Figura_Geometrica
+    {
+        protected int Base_1 = 0;
+        protected int Altura_1 = 0;
+
+        public void SetBase(int Base_1)
+        {
+            this.Base_1 = Base_1;
+
+        }
+
+        public void SetAltura(int Altura_1)
+        {
+            this.Altura_1 = Altura_1;
+
+        }
+
+
+
+    }
+}

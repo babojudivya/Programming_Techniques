8d3927de956545eee2740190fb4652a100afbac7
Erick Bárcenas <erick777wild@gmail.com>
2018-08-18 14:32:05 -0500

    carpeta con ejercicios de C_Gato y conceptos

---
+++ Anexos/Poo/Principal_Herencia/Principal_Herencia/Rectangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principal_Herencia
+{
+    public class Rectangulo: Figura_Geometrica
+    {
+
+
+        public int GetArea_()
+        {
+            return Base_1 * Altura_1;
+        }
+
+       
+
+
+    }
+}

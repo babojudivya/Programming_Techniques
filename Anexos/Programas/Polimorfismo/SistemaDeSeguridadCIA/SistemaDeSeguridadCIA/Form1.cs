3f1350389e2b49652ce41757d66cd2fec66faa4e
Erick Bárcenas <erick777wild@gmail.com>
2018-08-21 21:48:38 -0500

    Sistema de Seguridad - interfaz gráfica

---
+++ Anexos/Programas/Polimorfismo/SistemaDeSeguridadCIA/SistemaDeSeguridadCIA/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaDeSeguridadCIA
+{
+    public partial class Form1 : Form
+    {
+        Sensores S1 = new Sensores();
+        public Form1()
+        {
+            InitializeComponent();
+
+            //richTextBox1.Text = tipoUsuario;
+            richTextBox1.Text = "Acceso Denegado";
+            richTextBox1.Text = "Acceso Permitido";
+
+            
+        }
+
+        /*
+          Este método es para identificar el tipo de usuario
+             */
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            S1.TipodeUsuario1 = comboBox1.SelectedItem.ToString();
+        }
+
+        /*
+         * Este método es para identificar el tipo de sensor
+         */
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            S1.TipodeSensor1 = comboBox2.SelectedItem.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedItem.ToString() == "Iris")
+            {
+                S1.ID1 = textBox1.Text;
+            }
+            else if (comboBox2.SelectedItem.ToString() == "Dactilar")
+            {
+                S1.ID1 = textBox1.Text;
+            }
+            else if (comboBox2.SelectedItem.ToString() == "Voz")
+            {
+                S1.ID1 = textBox1.Text;
+            }
+            
+            S1.IdentificarUsuario();
+        }
+    }
+}
---
+++ Anexos/Programas/Polimorfismo/SistemaDeSeguridadCIA/SistemaDeSeguridadCIA/Sensores.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeSeguridadCIA
+{
+    class Sensores
+    {
+        private string ID;
+        private string TipodeUsuario;
+        private string TipodeSensor;
+        private string usuario;
+        protected bool Status;
+
+        public string TipodeUsuario1 { get => TipodeUsuario; set => TipodeUsuario = value; }
+        public string Usuario { get => usuario; set => usuario = value; }
+        public string ID1 { get => ID; set => ID = value; }
+        public string TipodeSensor1 { get => TipodeSensor; set => TipodeSensor = value; }
+
+        public void AbrirPuerta()
+        {
+
+        }
+
+        public void CerrarPuerta()
+        {
+
+        }
+
+        public void IdentificarUsuario()
+        {
+            if (TipodeUsuario == "Directivo")
+            {
+                Status = true;
+            }
+            else if ((TipodeUsuario == "Académico") && ((TipodeSensor == "Dactilar")||((TipodeSensor == "Voz"))))
+            {
+                Status = true;
+            }
+            else if ((TipodeUsuario == "Estudiante") && (TipodeSensor == "Voz")) 
+            {
+                Status = true;
+            }
+        }
+
+        public void DenegarAcceso()
+        {
+
+        }
+
+        public void ActivarAlarma()
+        {
+
+        }
+
+        public void Bloquear()
+        {
+
+        }
+        
+    }
+}

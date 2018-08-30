4e7eec4ee2097663a75f7daf50272c2647ca84b9
Erick I Bárcenas M <42219451+erickbarcenas@users.noreply.github.com>
2018-08-30 17:59:46 -0500

    Operadores

---
+++ Practica_4/Robot/Padre_Robot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot
+{
+    class Padre_Robot
+    {
+        //Double numero;
+
+        public Padre_Robot(string valor_Ingresado)
+        {
+            Frase = valor_Ingresado.Length;
+        }
+
+
+        public static int operator -(Padre_Robot x, Padre_Robot y)
+        {
+            return x.Frase + y.Frase;
+        }
+
+        public int Frase { get; set; }
+    }
+}
---
+++ Practica_4/Robot/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Padre_Robot Saludo = new Padre_Robot("one");
+
+            Padre_Robot Nombre = new Padre_Robot("one");
+
+
+            int Mensaje = Saludo - Nombre;
+
+            Console.WriteLine(Mensaje);
+            Console.Read();
+        }
+    }
+}
